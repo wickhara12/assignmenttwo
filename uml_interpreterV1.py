f4d5748b6ebf815c0fed197453f80637d044e7b8
wickhara12 <48776797+wickhara12@users.noreply.github.com>
2019-04-30 18:44:18 +1200

    long methods

--- uml_interpreterV1.py
+++ uml_interpreterV1.py
@@ -1,31 +1,30 @@
+"""this is Simon England's code for task five"""
+
 class UmlInterpreter:
     def __init__(self):
         self.uml_content = ['@startuml\n', '\n', 'class ClassName {\n', '}\n', '@enduml']
-        self.uml_list = []
-        self.class_dict = {}
-        self.attr_list = []
-        self.method_list = []
 
     def uml_decoder(self, uml_content: object) -> object:
+        uml_list = []
+        class_dict = {}
+        attr_list = []
+        method_list = []
         for line in uml_content:
             if 'class' in line:
-                self.class_dict['class'] = self.uml_class(line)
+                class_dict['class'] = self.uml_class(line)
             elif '(' in line and ')' in line:
-                self.method_list.append(self.uml_method(line))
+                method_list.append(self.uml_method(line))
             elif '}' in line:
-                UmlInterpreter.class_check(self)
+                class_dict['attribute'] = attr_list
+                class_dict['method'] = method_list
+                temp_dict = class_dict.copy()
+                uml_list.append(temp_dict)
+                class_dict.clear()
+                method_list = []
+                attr_list = []
             elif '(' not in line and ')' not in line and '@' not in line and len(line) > 1:
-                self.attr_list.append(self.uml_attribute(line))
-        return self.uml_list
-
-    def class_check(self):
-        self.class_dict['attribute'] = self.attr_list
-        self.class_dict['method'] = self.method_list
-        temp_dict = self.class_dict.copy()
-        self.uml_list.append(temp_dict)
-        self.class_dict.clear()
-        self.method_list = []
-        self.attr_list = []
+                attr_list.append(self.uml_attribute(line))
+        return uml_list
 
     def uml_class(self, new_line):
         remove_list = ['class', ' ', '{', '\n']
